9cf145b92f0ac5f04085f9eb696b942f626491e8
Paul Wolf <github@flowluap.com>
2025-09-13 19:01:29 +0200

    fix: added missing icons

---
+++ src/lib/shadcn/icons/check.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from '../utils';
+	import type { ClassValue } from 'clsx';
+
+	interface CheckProps {
+		class?: ClassValue;
+	}
+
+	let { class: className }: CheckProps = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class={cn('h-4 w-4', className)}
+>
+	<polyline points="20,6 9,17 4,12" />
+</svg>
---
+++ src/lib/shadcn/icons/chevron-down.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from '../utils';
+	import type { ClassValue } from 'clsx';
+
+	interface ChevronDownProps {
+		class?: ClassValue;
+	}
+
+	let { class: className }: ChevronDownProps = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class={cn('h-4 w-4', className)}
+>
+	<path d="m6 9 6 6 6-6" />
+</svg>
